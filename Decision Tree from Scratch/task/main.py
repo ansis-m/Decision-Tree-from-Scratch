6587602564f2e29a5523f3926ac6f917c5549948
ansis@home <ansis@home>
2023-09-21 12:52:36 +0300

    refractored a bit

--- Decision Tree from Scratch/task/main.py
+++ Decision Tree from Scratch/task/main.py
@@ -1,23 +1,78 @@
+import string
 import numpy as np
+import pandas as pd
+
+FILE_PATH = ".\\test\\data_stage2.csv"
 
 
-def get_input() -> np.array:
-    return np.array([int(x) for x in input().split(" ")])
+def gini_impurity(array) -> float:
+    array_len = len(array)
+    if array_len == 0:
+        return 0
+    array_sum = np.sum([int(x) for x in array])
+    return 1 - (array_sum / array_len) ** 2 - (1 - array_sum / array_len) ** 2
 
 
-def get_impurity(array: np.array) -> float:
-    return 1 - (np.sum(array) / len(array)) ** 2 - ((len(array) - np.sum(array)) / len(array)) ** 2
+class Gini:
+    def __init__(self, file_path: string):
+        self.df: pd.DataFrame = pd.read_csv(file_path)
+        self.df.set_index(self.df.columns[0], inplace=True)
+        self.df.index.name = 'index'
+        #print(self.df)
+        self.outcome: pd.Series = self.df.iloc[:, -1]
+        self.rows = len(self.df)
+        self.result = 1
+        self.index = -1
+        self.value = ""
+
+    def _run_splitting_function(self):
+        for i in range(0, self.df.shape[1] - 1):
+            gini, value = self._best_weighted_gini_for_column(i)
+            if gini < self.result:
+                self.result = gini
+                self.index = i
+                self.value = value
+        return self
+
+    def _best_weighted_gini_for_column(self, index: int) -> tuple:
+        column: pd.Series = self.df.iloc[:, index]
+        result: float = 1.0
+        value: string = ""
+        unique = column.unique()
+
+        for entry in unique:
+            this_result = self.get_weighted_gini(entry, column)
+            if this_result < result:
+                result = this_result
+                value = entry
+
+        return result, value
+
+    def print_results(self):
+        self._run_splitting_function()
+        print("{} {} {} {} {}".format(
+            self.result,
+            self.df.columns[self.index],
+            self.value,
+            self.df[self.df.iloc[:, self.index] == self.value].index.tolist(),
+            self.df[self.df.iloc[:, self.index] != self.value].index.tolist()
+
+        ))
+
+    def get_weighted_gini(self, entry, column) -> float:
+        matching = self.outcome[column == entry]
+        not_matching = self.outcome[column != entry]
+
+        gini_matching = gini_impurity(matching)
+        gini_not_matching = gini_impurity(not_matching)
+        return len(matching) / self.rows * gini_matching + len(not_matching) / self.rows * gini_not_matching
 
 
-def weighted_gini(array1: np.array, array2: np.array) -> float:
-    size = len(array1) + len(array2)
-    return len(array1) / size * get_impurity(array1) + len(array2) / size * get_impurity(array2)
+def main():
+    FILE_PATH = input()
+    gini = Gini(FILE_PATH)
+    gini.print_results()
 
 
 if __name__ == "__main__":
-    node: np.array = get_input()
-    split_1: np.array = get_input()
-    split_2: np.array = get_input()
-    impurity = get_impurity(node)
-    weighted_impurity = weighted_gini(split_1, split_2)
-    print("{:.5f}".format(impurity), "{:.5f}".format(weighted_impurity))
+    main()
