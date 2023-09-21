7a55b116841bbb45dce251079cb5f37691723000
ansis@home <ansis@home>
2023-09-21 12:55:00 +0300

    next

--- Decision Tree from Scratch/task/test/tests.py
+++ Decision Tree from Scratch/task/test/tests.py
@@ -1,10 +1,10 @@
 from hstest import *
-import ast
-
-ANSWER = [0.0, 'Sex', 0, [0, 4, 5, 6, 7], [1, 2, 3, 8, 9]]
 
 
-class SplitTest(StageTest):
+ANSWER = ["Made split: Sex is 0", "Made split: Pclass is 2", "Made split: Pclass is 3"]
+
+
+class RecSplitTest(StageTest):
 
     @dynamic_test
     def test(self):
@@ -12,49 +12,23 @@ class SplitTest(StageTest):
         pr.start()
         if not pr.is_waiting_input():
             raise WrongAnswer("You program should input the path to the file")
-        output = pr.execute("test/data_stage2.csv").strip().replace(", ", ",")
-        output = [x.strip() for x in output.split()]
-        if len(output) != 5:
-            raise WrongAnswer("Your answer should contain five items split by a space: a float, a string, an integer, "
-                              "and two lists.")
-
-        # checking the first item
-        try:
-            user_gini = float(output[0])
-        except Exception:
-            raise WrongAnswer("The first value should be a float.")
-        if user_gini != ANSWER[0]:
-            raise WrongAnswer("Wrong Gini score (the first item in your answer).")
-
-        # checking the second item
-        if output[1] != ANSWER[1]:
-            raise WrongAnswer("Wrong threshold feature (the second item in your answer).")
-
-        # checking the third item
-        try:
-            user_threshold = int(output[2])
-        except Exception:
-            raise WrongAnswer("The third value should be an integer.")
-        if user_threshold != ANSWER[2]:
-            raise WrongAnswer("Wrong threshold value (the third item in your answer).")
-
-        # checking the forth and the fifth item
-        for i, ans, node in zip([0, 1], output[3:], ["left", "right"]):
-            index_from = ans.find('[')
-            index_to = ans.find(']')
-            list_str = ans[index_from: index_to + 1]
-            try:
-                user_list = ast.literal_eval(list_str)
-            except Exception:
-                return CheckResult.wrong(f"Seems that the {4 + i}th item of your output is in wrong format. The list is expected.")
-
-            if not isinstance(user_list, list):
-                return CheckResult.wrong(f'Print the {4 + i}th item as a list')
-            if user_list != ANSWER[3+i]:
-                raise WrongAnswer(f"Wrong list of {node} node indexes (the {4 + i}th item in your answer).")
-
+        output = pr.execute("test/data_stage3.csv").strip()
+        res = output.split("\n")
+        res = [x.strip() for x in res]
+        if len(res) != 3:
+            raise WrongAnswer("Wrong number of splits. Your function should make a split three times (on given data)."
+                              "\nIt may be useful to check the traversal order in the recursive split function: go to the left child first.")
+        if res[0] != ANSWER[0]:
+            raise WrongAnswer("Wrong first log message. Correct message template: 'Made split: Sex is 1'."
+                              "\nIt may be useful to check the traversal order in the recursive split function: go to the left child first.")
+        if res[1] != ANSWER[1]:
+            raise WrongAnswer("Wrong second log message. Correct message template: 'Made split: Sex is 1'."
+                              "\nIt may be useful to check the traversal order in the recursive split function: go to the left child first.")
+        if res[2] != ANSWER[2]:
+            raise WrongAnswer("Wrong third log message. Correct message template: 'Made split: Sex is 1'."
+                              "\nIt may be useful to check the traversal order in the recursive split function: go to the left child first.")
         return CheckResult.correct()
 
 
 if __name__ == '__main__':
-    SplitTest().run_tests()
+    RecSplitTest().run_tests()
--- Decision Tree from Scratch/task/tests.py
+++ Decision Tree from Scratch/task/tests.py
@@ -1,3 +1,9 @@
-from test.tests import SplitTest
+<<<<<<< Updated upstream
+from test.tests import FuncTest
 
-if __name__ == '__main__':    SplitTest().run_tests()
+if __name__ == '__main__':    FuncTest().run_tests()
+=======
+from test.tests import RecSplitTest
+
+if __name__ == '__main__':    RecSplitTest().run_tests()
+>>>>>>> Stashed changes
