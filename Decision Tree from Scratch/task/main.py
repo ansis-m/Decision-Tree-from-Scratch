82ee81fabbdf1edb4ede4a5d1878b464aa269d64
ansis@home <ansis@home>
2023-09-22 12:45:06 +0300

    completed stage 3

--- Decision Tree from Scratch/task/main.py
+++ Decision Tree from Scratch/task/main.py
@@ -2,13 +2,14 @@ import numpy as np
 import pandas as pd
 
 FILE_PATH = ".\\test\\data_stage31.csv"
+MAX_GINI = 1.0
 
 
 def gini_impurity(array) -> float:
     array_len = len(array)
     if array_len == 0:
         return 0
-    array_sum = np.sum([int(x) for x in array])
+    array_sum = np.sum(array)
     return 1 - (array_sum / array_len) ** 2 - (1 - array_sum / array_len) ** 2
 
 
@@ -16,37 +17,29 @@ def construct_tree(df: pd.DataFrame):
     #print(df)
     outcome: pd.Series = df.iloc[:, -1]
     rows = len(df)
-    gini = 1
+    gini = MAX_GINI
     index = -1
     value = ""
 
     def _best_weighted_gini_for_column(index: int) -> tuple:
         column: pd.Series = df.iloc[:, index]
-        column_result: float = 1.0
+        column_gini: float = MAX_GINI
         column_value: str = ""
         unique = column.unique()
 
         for entry in unique:
-            this_result = get_weighted_gini(entry, column)
-            if this_result < column_result:
-                column_result = this_result
+            this_gini = get_weighted_gini(entry, column)
+            if this_gini < column_gini:
+                column_gini = this_gini
                 column_value = entry
 
-        return column_result, column_value
+        return column_gini, column_value
 
     def print_results():
         print("Made split: {} is {}".format(
             df.columns[index],
             value,
         ))
-        left_node = df[df.iloc[:, index] == value]
-        right_node = df[df.iloc[:, index] != value]
-        if gini_impurity(left_node[left_node.columns[-1]]) != 0 and left_node.shape[0] > 1 and not_redundant(left_node):
-            construct_tree(left_node)
-
-        if gini_impurity(right_node[right_node.columns[-1]]) != 0 and right_node.shape[0] > 1 and not_redundant(
-                right_node):
-            construct_tree(right_node)
 
     def get_weighted_gini(entry, column) -> float:
         matching = outcome[column == entry]
@@ -68,6 +61,13 @@ def construct_tree(df: pd.DataFrame):
             value = column_value
 
     print_results()
+    left_node = df[df.iloc[:, index] == value]
+    right_node = df[df.iloc[:, index] != value]
+    if gini_impurity(left_node[left_node.columns[-1]]) != 0 and left_node.shape[0] > 1 and not_redundant(left_node):
+        construct_tree(left_node)
+    if gini_impurity(right_node[right_node.columns[-1]]) != 0 and right_node.shape[0] > 1 and not_redundant(
+        right_node):
+        construct_tree(right_node)
 
 
 def main():
