c9d71cd7c7e0045216c58f45d92fd1d2421d17f9
ansis@home <ansis@home>
2023-09-23 11:49:44 +0300

    completed stage 6

--- Decision Tree from Scratch/task/test/tests.py
+++ Decision Tree from Scratch/task/test/tests.py
@@ -1,22 +1,13 @@
 from hstest import *
 
 
-ANSWER = ['Prediction for sample # 0',
-          'Considering decision rule on feature Sex with value 0',
-          'Considering decision rule on feature SibSp with value 0',
-          'Considering decision rule on feature Pclass with value 3',
-          'Considering decision rule on feature Parch with value 0',
-          'Predicted label: 1',
-          'Prediction for sample # 1',
-          'Considering decision rule on feature Sex with value 0',
-          'Considering decision rule on feature Pclass with value 2',
-          'Considering decision rule on feature Parch with value 1',
-          'Considering decision rule on feature SibSp with value 0',
-          'Considering decision rule on feature Pclass with value 3',
-          'Predicted label: 0']
+ANSWER = [0.554, 0.612, 0.857, 0.948]
+
+# in ANSWER there are lower and upper bounds
+# the correct answer is 0.583, 0.903
 
 
-class TreePredTest(StageTest):
+class Eval1Test(StageTest):
 
     @dynamic_test
     def test(self):
@@ -24,30 +15,20 @@ class TreePredTest(StageTest):
         pr.start()
         if not pr.is_waiting_input():
             raise WrongAnswer("You program should input two paths to the files split by space")
-        output = pr.execute("test/data_stage5_train.csv test/data_stage5_test.csv").strip()
-        res = output.split("\n")
-        res = [x.strip() for x in res]
-        if len(res) != 13:
-            raise WrongAnswer("Wrong number of log messages.\n"
-                              "Your model should visit four internal nodes making prediction for the first sample\n"
-                              "and five internal nodes making prediction for the second sample.\n"
-                              "Do not forget to add log messages showing the number of sample and its predicted label.")
-        for i in range(1, 13):
-            if i == 6:
-                continue
-            if res[i] != ANSWER[i]:
-                if i == 5:
-                    raise WrongAnswer("Wrong prediction for the first sample."
-                                      "\nCorrect message template: 'Predicted label: 0'.")
-                elif i == 12:
-                    raise WrongAnswer("Wrong prediction for the second sample."
-                                      "\nCorrect message template: 'Predicted label: 0'.")
-                else:
-                    raise WrongAnswer(f"Wrong log message on line {i+1}."
-                                      "\nIt may be useful to check the traversal order in the recursive split function: go to the left child first."
-                                      "\nCorrect message template: 'Considering decision rule on feature Sex with value 1'.")
+        output = pr.execute("test/data_stage6_train.csv test/data_stage6_test.csv").strip()
+        try:
+            res = [round(float(x), 3) for x in output.split()]
+        except Exception:
+            raise WrongAnswer("You should print two float values split with space.")
+        if len(res) != 2:
+            raise WrongAnswer("Wrong number of values. Print two numbers: true positives and true negatives normalized over the true rows.")
+        if not (ANSWER[0] <= res[0] <= ANSWER[1]):
+            raise WrongAnswer("Wrong true positives value (the first value).")
+        if not (ANSWER[2] <= res[1] <= ANSWER[3]):
+            raise WrongAnswer("Wrong true negatives value (the second value).")
         return CheckResult.correct()
 
 
 if __name__ == '__main__':
-    TreePredTest().run_tests()
+    Eval1Test().run_tests()
+
